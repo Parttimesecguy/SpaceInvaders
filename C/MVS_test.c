22ea0bc4c1d83e09488b9e303a724fcb751f0b21
cassidyc <cassidyc@btinternet.com>
2024-02-17 12:47:26 +0000

    Added C Code Example

---
+++ C/MVS_test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// intadmin contains a Mongolian Vowel Separator U+180E
+
+int main (void)
+{
+	int intadmin = 1;
+	// clear the admin flag
+	int᠎admin = 0;
+	
+	if(intadmin == 1)
+	{
+		printf("you are admin\n");
+	}
+	return 0;
+
+}
---
+++ C/ZWS_test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// intadmin contains a Zero Width Space U+200B
+
+int main (void)
+{
+	int intadmin = 1;
+	// clear the admin flag
+	int​admin = 0;
+	
+	if(intadmin == 1)
+	{
+		printf("you are admin\n");
+	}
+	return 0;
+
+}
