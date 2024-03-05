6ad844d8a575ebe8399a5d42e60fdd7ab033ee7d
cassidyc <cassidyc@btinternet.com>
2024-03-05 10:27:38 +0000

    Added Java Code

---
+++ Java/MVC_Testing.java
@@ -0,0 +1,13 @@
+
+public class MVC_Testing {
+
+	public static void main(String[] args) {
+		int intadmin = 1;
+		// clear the intadmin flag
+		int᠎admin = 0;
+		if(intadmin == 1)
+		{
+			System.out.println("you are admin\n"); 
+		}
+	}
+}
---
+++ Java/ZWS_Testing.java
@@ -0,0 +1,13 @@
+
+public class MVC_Testing {
+
+	public static void main(String[] args) {
+		int intadmin = 1;
+		// clear the intadmin flag
+		int​admin = 0;
+		if(intadmin == 1)
+		{
+			System.out.println("you are admin\n"); 
+		}
+	}
+}
