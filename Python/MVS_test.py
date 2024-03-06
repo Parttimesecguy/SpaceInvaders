defcb9636293db39f13f70a5fd22bb065c77802a
cassidyc <cassidyc@btinternet.com>
2024-03-06 10:19:07 +0000

    Added Python

---
+++ Python/MVS_test.py
@@ -0,0 +1,6 @@
+admin = 1
+#clear the admin flag
+adm᠎in = 0
+if admin == 1:
+	print("you are admin")
+	
---
+++ Python/ZWS_test.py
@@ -0,0 +1,6 @@
+admin = 1
+#clear the admin flag
+adm​in = 0
+if admin == 1:
+	print("you are admin")
+	
