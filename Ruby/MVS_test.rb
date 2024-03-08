d71f771b2c59f4b2722d4a619e63ab3c2c2ebb6f
cassidyc <cassidyc@btinternet.com>
2024-03-08 10:27:32 +0000

    Ruby

---
+++ Ruby/MVS_test.rb
@@ -0,0 +1,6 @@
+@admin = 1;
+# clear the adminflag
+@adm᠎in= 0;
+if(@admin == 1)
+	puts("you are admin");
+end
---
+++ Ruby/ZWS_test.rb
@@ -0,0 +1,6 @@
+@admin = 1;
+# clear the adminflag
+@adm​in = 0;
+if(@admin == 1)
+	puts("you are admin");
+end
