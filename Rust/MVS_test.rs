3f22a8e8e58bb75e8c42c7c6acc8863a48165402
cassidyc <cassidyc@btinternet.com>
2024-03-11 10:56:10 +0000

    Added Rust

---
+++ Rust/MVS_test.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut letisadmin: bool = true;
+    
+    // Clear the isadmin flag
+    let᠎isadmin = false;
+    
+    if letisadmin == true
+    {
+    	println!("You are admin");
+    }
+}
---
+++ Rust/ZWS_test.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut letisadmin: bool = true;
+    
+    // Clear the isadmin flag
+    let​isadmin = false;
+    
+    if letisadmin == true
+    {
+    	println!("You are admin");
+    }
+}
